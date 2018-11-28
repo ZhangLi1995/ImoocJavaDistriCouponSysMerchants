4fa0317d95a710f30faa388dc06a52d87780db5c
zhangli <diankezhangli@qq.com>
2018-11-28 11:24:55 +0800

    9th commit merchants: 商户投放子系统-创建商户服务接口实现

---
+++ src/test/java/com/imooc/passbook/merchants/service/MerchantsServTest.java
@@ -0,0 +1,38 @@
+package com.imooc.passbook.merchants.service;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.passbook.merchants.vo.CreateMerchantsRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**<h1>商户服务测试类</h1>
+ * Created by Zhangli.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class MerchantsServTest {
+
+    @Autowired
+    private IMerchantsServ merchantsServ;
+
+    /** {"data":{"id":17}}
+     *  {"data":{"id":18}}
+     */
+    @Test
+    @Transactional
+    public void testCreateMerchantServ() {
+
+        CreateMerchantsRequest request = new CreateMerchantsRequest();
+        request.setName("慕课");
+        request.setLogoUrl("www.imooc.com");
+        request.setBusinessLicenseUrl("www.imooc.com");
+        request.setPhone("1234567890");
+        request.setAddress("北京市");
+
+        System.out.println(JSON.toJSONString(merchantsServ.createMerchants(request)));
+    }
+}
